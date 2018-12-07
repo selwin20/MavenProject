7638f4bdc229ad4514028bc02c3f91b908ca14e2
selwin20 <sel2012maza@gmail.com>
2018-12-06 21:58:58 -0500

    first commit

---
+++ src/test/java/com/syntax/reviewproject/MavenTest.java
@@ -0,0 +1,14 @@
+package com.syntax.reviewproject;
+
+import org.testng.annotations.Test;
+
+public class MavenTest {
+	
+	
+	@Test
+	public void hello() {
+		System.out.println("hello! this is a Maven Test.");
+	}
+	
+
+}
---
+++ src/test/java/com/syntax/reviewproject/MavenTest2.java
@@ -0,0 +1,12 @@
+package com.syntax.reviewproject;
+
+import org.testng.annotations.Test;
+
+public class MavenTest2 {
+	
+	@Test
+	public void hello2() {
+		System.out.println("hello2! this is a Maven Test.");
+	}
+
+}
